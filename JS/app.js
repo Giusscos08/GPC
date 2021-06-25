ff485225bd13fe18e9b804ca9044eeaa1edf0ca1
Giuseppe Pio Cosenza <63466096+Giusscos08@users.noreply.github.com>
2021-06-25 12:54:02 +0200

    Add files via upload

--- JS/app.js
+++ JS/app.js
@@ -1,19 +1,8 @@
 $(document).ready(function() {
-    //Zoom on scroll
-    $(window).scroll(function(){
-        var scroll = $(window).scrollTop();
-        $(".zoom img").css({
-            width: (100 + scroll/2) + "%",
-        })    
-    });
-    //Opacity low on scroll
-    $(window).scroll(function(){
-        $(".zoom").css("opacity", 1 - $(window).scrollTop()/700)
-    })
     //Cursore magnetico
-    VanillaTilt.init(document.querySelector(".boSet"), {
+    VanillaTilt.init(document.querySelectorAll(".ctn-icn"), {
 		max: 25,
 		speed: 400
 	});
-    VanillaTilt.init(document.querySelectorAll(".boSet"));
+    VanillaTilt.init(document.querySelectorAll(".ctn-icn"));
 })
